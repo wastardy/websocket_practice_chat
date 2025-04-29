2a0fa6dd131dcce34bb8b0c56ace7d2917367f98
Andrew Kononenko <wastardy.k@gmail.com>
2025-04-29 17:22:40 +0300

    chore: add Character type definition for character data structure

---
+++ character.type.ts
@@ -0,0 +1,10 @@
+export type Character = {
+  id: number;
+  name: string;
+  stand: string;
+  first_appearance: string;
+  fighting_style: string;
+  notable_enemy: string;
+  personality: string;
+  image: string;
+};
