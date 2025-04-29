fba9b2b29d6159d4667b6e56c6eccb668b8c51f1
Andrew Kononenko <wastardy.k@gmail.com>
2025-04-29 13:18:03 +0300

    chore: add images for characters in the dashboard

--- characters_dashboard.ts
+++ characters_dashboard.ts
@@ -7,6 +7,8 @@ export const characters = [
     fighting_style: "Martial Arts",
     notable_enemy: "Dio Brando",
     personality: "Noble and honorable",
+    image:
+      "https://i.pinimg.com/736x/59/49/d6/5949d659179432a954ce90b8dc62b3cc.jpg",
   },
   {
     id: 2,
@@ -16,6 +18,8 @@ export const characters = [
     fighting_style: "Vampiric Power",
     notable_enemy: "Jonathan Joestar",
     personality: "Cunning and manipulative",
+    image:
+      "https://i.pinimg.com/736x/3a/4e/fa/3a4efa5b7078560335146371624bd333.jpg",
   },
   {
     id: 3,
@@ -25,6 +29,8 @@ export const characters = [
     fighting_style: "Clever and trickery-based",
     notable_enemy: "Straizo",
     personality: "Quick-witted and humorous",
+    image:
+      "https://i.pinimg.com/736x/fd/1a/0c/fd1a0c8f5afe519cd31db1c7598fd794.jpg",
   },
   {
     id: 4,
@@ -34,6 +40,8 @@ export const characters = [
     fighting_style: "Strong, direct combat",
     notable_enemy: "Dio Brando",
     personality: "Stoic and serious",
+    image:
+      "https://i.pinimg.com/564x/3b/b4/fa/3bb4faf483d7d7c63ecec6c482941de6.jpg",
   },
   {
     id: 5,
@@ -43,6 +51,8 @@ export const characters = [
     fighting_style: "Precision and agility",
     notable_enemy: "Dio Brando",
     personality: "Loyal and reserved",
+    image:
+      "https://i.pinimg.com/736x/46/21/bc/4621bc94786c4aba379f2ab280bc500f.jpg",
   },
   {
     id: 6,
@@ -52,6 +62,8 @@ export const characters = [
     fighting_style: "Fast-paced sword fighting",
     notable_enemy: "Dio Brando",
     personality: "Brave and chivalrous",
+    image:
+      "https://i.pinimg.com/474x/95/70/2a/95702abc61d6132ef4a61af55faf8c3d.jpg",
   },
   {
     id: 7,
@@ -61,6 +73,8 @@ export const characters = [
     fighting_style: "Fire-based attack",
     notable_enemy: "Dio Brando",
     personality: "Strong and wise",
+    image:
+      "https://i.pinimg.com/736x/e5/f9/9a/e5f99a4a467170500cede94843a2ab2b.jpg",
   },
   {
     id: 8,
@@ -70,6 +84,8 @@ export const characters = [
     fighting_style: "Psychic manipulation through writing",
     notable_enemy: "Kira Yoshikage",
     personality: "Arrogant but artistic",
+    image:
+      "https://i.pinimg.com/474x/38/07/52/3807527494a912357a8f6be38c4610a8.jpg",
   },
   {
     id: 9,
@@ -79,6 +95,8 @@ export const characters = [
     fighting_style: "Life-manipulating power",
     notable_enemy: "Diavolo",
     personality: "Ambitious and determined",
+    image:
+      "https://i.pinimg.com/474x/8e/1b/64/8e1b6490e8536658eb2363f415b3e21a.jpg",
   },
   {
     id: 10,
@@ -88,5 +106,7 @@ export const characters = [
     fighting_style: "Spatial manipulation and speed",
     notable_enemy: "Diavolo",
     personality: "Loyal and empathetic",
+    image:
+      "https://i.pinimg.com/474x/98/5c/eb/985ceb5db469d201029e38b9479e2759.jpg",
   },
 ];
